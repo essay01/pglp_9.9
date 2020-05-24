3b41e864b5c1dca9e0214666da076eb8f5857c3a
myfirstOne <meryem.elmchichi@gmail.com>
2020-05-24 21:15:09 +0200

    commit

---
+++ src/main/java/uvsq/commandes/Command.java
@@ -0,0 +1,6 @@
+package uvsq.commandes;
+
+public interface Command {
+
+	public void execute();
+}
---
+++ src/main/java/uvsq/commandes/QuitterCommand.java
@@ -0,0 +1,18 @@
+package uvsq.commandes;
+
+import static java.lang.System.exit;
+
+import uvsq.connection.DBConnection;
+
+public class QuitterCommand implements Command {
+
+	public QuitterCommand() {
+	}
+
+	/** la fermeture de la bd et l'arret de l'application */
+	@Override
+	public void execute() {
+		DBConnection.close();
+		exit(0);
+	}
+}
